e7bf56c05520476b2cdfab8c31e26da648cec9c2
Alexandre-Vernet <alexandre.vernet@g-mail.fr>
2020-10-30 20:59:24 +0100

    Jouer un son à la secousse du téléphone

--- app/src/main/java/com/ynov/vernet/cloche/MainActivity.java
+++ app/src/main/java/com/ynov/vernet/cloche/MainActivity.java
@@ -5,8 +5,8 @@ import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
+import android.media.MediaPlayer;
 import android.os.Bundle;
-import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -18,11 +18,17 @@ public class MainActivity extends AppCompatActivity {
     private float mAccelCurrent;
     private float mAccelLast;
 
+    MediaPlayer mediaPlayerCloche;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // Son
+        mediaPlayerCloche = MediaPlayer.create(this, R.raw.cloche);
+
+        // Gestion du capteur
         mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
         Objects.requireNonNull(mSensorManager).registerListener(mSensorListener, mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER),
                 SensorManager.SENSOR_DELAY_NORMAL);
@@ -41,9 +47,8 @@ public class MainActivity extends AppCompatActivity {
             mAccelCurrent = (float) Math.sqrt((double) (x * x + y * y + z * z));
             float delta = mAccelCurrent - mAccelLast;
             mAccel = mAccel * 0.9f + delta;
-            if (mAccel > 12) {
-
-            }
+            if (mAccel > 12)
+                mediaPlayerCloche.start();
         }
 
         @Override
